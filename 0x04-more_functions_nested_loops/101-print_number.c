3d733efb07879b8c01ecbe3790f1ab37b6ca2adb
Olorunbunmi Olajumoke <62801575+olajay-pro@users.noreply.github.com>
2021-06-24 21:16:10 +0100

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <stdio>
 
 /**
 * print_number - prints # using _putchar function
@@ -7,28 +8,14 @@
 
 void print_number(int n)
 {
-	int copy, nth, size = 1, ones = n % 10;
-
-	n /= 10;
-	copy = n;
-	if (ones < 0)
+	if (n != 0)
 	{
-		ones *= -1, copy *= -1, n *= -1;
-		_putchar('-');
+		print_number (n / 10);
+		_putchar((n % 10) + '0');
 	}
-	if (copy > 0)
-	{
-		while (copy / 10 != 0)
-		{
-			copy /= 10, size *= 10;
-		}
-		while (size > 0)
-		{
-			nth = n / size;
-			_putchar('0' + nth);
-			n -= nth * size;
-			size /= 10;
-		}
-	}
-	_putchar('0' + ones);
+}
+main(void)
+{
+	int a = 84571;
+	print_number(a);
 }
