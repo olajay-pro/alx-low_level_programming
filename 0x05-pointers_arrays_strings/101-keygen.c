f83cb59b4ca6f29d93c8470a7f782d1e8e7a59a7
Olorunbunmi Olajumoke <62801575+olajay-pro@users.noreply.github.com>
2021-06-28 15:42:47 +0100

    Generates password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,27 +4,25 @@
 #include <time.h>
 #include <math.h>
 
-int main()
+int main(void) 
 {
-
-    int i;
-    char pass[12];
-    char capLetter = 'A' + (rand() % 26);
-    char letter = 'a' + (rand() % 26);
     srand((unsigned int)(time(NULL)));
+    int i;
+    char pass[13];
 
     printf("Press enter to get a twelve-character password\n");
     getchar();
 
-    for (i = 0; i < 4; i++) {
-        pass[i] = rand() % 9;
-        
-        pass[i + 2] = capLetter;
-        
-        pass[i + 3] = letter;
-        printf("%d%c%c", pass[i], pass[i + 2], pass[i + 3]);
+    for (i = 0; i < 4; i++) 
+    {
+        pass[ 3 * i ] = '0' + (rand() % 10); 
+        char capLetter = 'A' + (rand() % 26); 
+        pass[(3 * i) + 1] = capLetter;
+        char letter = 'a' + (rand() % 26); 
+        pass[(3 * i) + 2] = letter;
     }
+    pass[3 * i] = '\0'; 
+    printf("generated password : %s\n\n",pass); 
+
     printf("\n\n");
-    
-    return (0);
 }
