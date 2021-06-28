a7ab0b448307355d4d8b4940df60e3d21a48f8cb
Olorunbunmi Olajumoke <62801575+olajay-pro@users.noreply.github.com>
2021-06-28 14:41:59 +0100

    Pointers and Arrays

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,31 @@
-#include "holberton"
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ *rev_string - reverses a string
+ *@s: string to be reversed
+ *Return: not.
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+	int l = 0;
+	char *y = s;
+	int e = 0;
+	int x;
+	char n;
+
+	while (*y != '\0')
+	{
+		y++;
+		len++;
+	}
+	l = len - 1;
+	for ( ; e < ((l / 2) + 1) ; e++)
+	{
+		x = (l - e);
+		n = s[e];
+		s[e] = s[x];
+		s[x] = n;
+	}
+}
