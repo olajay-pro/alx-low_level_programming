db184dd2a49165688e482383e810fa006abe37f5
Olorunbunmi Olajumoke <62801575+olajay-pro@users.noreply.github.com>
2021-06-28 11:44:04 +0100

    initial commit

--- 0x05-pointers_arrays_strings/0-main.c
+++ 0x05-pointers_arrays_strings/0-main.c
@@ -1,1 +1,17 @@
+#include "holberton.h"
 #include <stdio.h>
+
+/**
+ * main - code checker
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	n = 402;
+	printf("n=%d\n", n);
+	reset_to_98(&n);
+	printf("n=%d\n", n);
+	return (0);
+}
