be2093e9f635b1d2fb89c822e48ca567c551e50a
Olorunbunmi Olajumoke <62801575+olajay-pro@users.noreply.github.com>
2021-06-28 15:44:30 +0100

    Generates password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,9 +6,11 @@
 
 int main(void) 
 {
-    srand((unsigned int)(time(NULL)));
     int i;
     char pass[13];
+     char capLetter = 'A' + (rand() % 26); 
+     char letter = 'a' + (rand() % 26); 
+    srand((unsigned int)(time(NULL)));
 
     printf("Press enter to get a twelve-character password\n");
     getchar();
@@ -16,13 +18,15 @@ int main(void)
     for (i = 0; i < 4; i++) 
     {
         pass[ 3 * i ] = '0' + (rand() % 10); 
-        char capLetter = 'A' + (rand() % 26); 
+       
         pass[(3 * i) + 1] = capLetter;
-        char letter = 'a' + (rand() % 26); 
+       
         pass[(3 * i) + 2] = letter;
     }
     pass[3 * i] = '\0'; 
     printf("generated password : %s\n\n",pass); 
 
     printf("\n\n");
+    
+    return (0);
 }
