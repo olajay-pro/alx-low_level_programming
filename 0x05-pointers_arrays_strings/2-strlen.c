f763e2f89a8c854568850196db883760c94bbed7
Olorunbunmi Olajumoke <62801575+olajay-pro@users.noreply.github.com>
2021-06-28 14:24:50 +0100

    Pointers and Arrays

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,20 @@
-#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
