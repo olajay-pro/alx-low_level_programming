e1439d7182febb5b13ba5e27a48c5e3817b3d1db
Olorunbunmi Olajumoke <62801575+olajay-pro@users.noreply.github.com>
2021-06-28 14:27:41 +0100

    Pointers and Arrays

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,15 @@
 #include "holberton.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: string to print
+ *Return: not.
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+		_putchar(*str++);
+
+	_putchar('\n');
+}
