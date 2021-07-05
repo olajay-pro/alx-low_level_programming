7a61dc03916ed9135597fdc62d34009e3f7b666f
Olorunbunmi Olajumoke <62801575+olajay-pro@users.noreply.github.com>
2021-07-05 20:18:51 +0100

    Updated code

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#define NULL 0
 
 /**
  * _strpbrk - return pointer to byte in s that matches a byte in accept
@@ -10,21 +9,18 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int i = 0, j;
+	int i;
 
-	while (s[i] != '\0') 
+	while (*s)
 	{
 
-		for (j = 0; accept[j] != '\0'; j++)
+		for (i = 0; accept[i]; i++)
 		{
-			if (s[i] == accept[j]) 
-			{
-				s = &s[i]; 
+			if (*s == accept[i])
 				return (s);
 			}
-		}
-		i++;
+		s++;
 	}
-	return (NULL); 
+	return ('\0');
 
 }
