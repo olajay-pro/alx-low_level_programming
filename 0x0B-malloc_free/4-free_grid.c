e665ccd79ab7b8a0c03cd93881386c41eabfe08a
Olorunbunmi Olajumoke <62801575+olajay-pro@users.noreply.github.com>
2021-07-13 18:16:23 +0100

    Initial Commit

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,1 +1,18 @@
+#include <stdlib.h>
 #include "holberton.h"
+
+/**
+ * free_grid - frees a 2 dimensional array of integers
+ * @grid: pointer to 2D array to be freed
+ * @height: height of array
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}
