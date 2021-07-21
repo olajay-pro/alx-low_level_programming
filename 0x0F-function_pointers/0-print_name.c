8d1d5fdf1c1e7b7d7b7ea11e7523683196284297
Olorunbunmi Olajumoke <62801575+olajay-pro@users.noreply.github.com>
2021-07-21 19:41:25 +0100

    betty style

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,16 @@
 #include <stdlib>
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - prints a name
+ * @name: pointer to char
+ * @f: pointer to function that returns nothing
+ * Return: nothing
+ **/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && *name != '\0' && f != NULL)
+	f(name);
+}
