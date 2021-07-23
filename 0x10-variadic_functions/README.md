f2073dafbb53fd962e30fccc2e985885035706c6
olajay-pro <olorun208@gmail.com>
2021-07-23 04:10:02 -0700

    Initial

---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+__This is file is mandatory__
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <stdarg.h>
+typedef struct printer
+{
+	char *symbol;
+	void (*print)(va_list arg);
+
+}printer_t;
+
+int sum_them_all(const unsigned int n, ...);
