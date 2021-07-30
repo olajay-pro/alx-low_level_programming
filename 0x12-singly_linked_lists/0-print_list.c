05105ea5dacc2a7b4f59af675aa3dd452ed71263
Olorunbunmi Olajumoke <62801575+olajay-pro@users.noreply.github.com>
2021-07-30 12:38:49 +0100

    fixed loop

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include "lists.h"
-#include <stdlib.h>
+
 /**
  * print_list - prints all elements of a list_t list
  * @h: singly linked list to print
  * Return: number of nodes in the list
  */
+
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
