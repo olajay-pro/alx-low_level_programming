72739cd7d265ac0ccbb6df080bfb665a4324e39c
Olorunbunmi Olajumoke <62801575+olajay-pro@users.noreply.github.com>
2021-07-30 12:19:31 +0100

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,12 +8,18 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	size_t count = 0;
 
-	for (i = 0; h; i++)
+	while (h != NULL)
 	{
-		printf("[%d] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		count++;
 		h = h->next;
 	}
-	return (i);
+
+	return (count);
 }
