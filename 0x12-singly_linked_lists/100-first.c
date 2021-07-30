1985b23f7ae587cda964db102f0a04751a63df38
Olorunbunmi Olajumoke <62801575+olajay-pro@users.noreply.github.com>
2021-07-30 13:03:17 +0100

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  */
 
 void execute_before_main(void) __attribute__ ((constructor));
-void execute_before_main(void)
+
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("Ï bore my house upon my back!\n");
