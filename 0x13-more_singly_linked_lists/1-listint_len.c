897d2c94127e7b0cd8ec12e3cbaafdb990ed1d69
Olorunbunmi Olajumoke <62801575+olajay-pro@users.noreply.github.com>
2021-08-03 19:56:24 +0100

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,20 +1,14 @@
 #include "lists.h"
-
 /**
- *listint_len - returns the number of elements in a linked list
- *@h: list
- *Return: number of nodes in list
+ * listint_len - returns the number of elements in a linked listint_t list
+ * @h: singly linked list
+ * Return: number of nodes
  */
-
 size_t listint_len(const listint_t *h)
 {
-	size_t num_nodes = 0;
+	size_t i;
 
-	while (h != NULL)
-	{
-		num_node += 1;
+	for (i = 0; h; i++)
 		h = h->next;
-	}
-
-	return (num_nodes);
+	return (i);
 }
