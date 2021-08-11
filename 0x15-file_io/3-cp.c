01d47901cd57832f692cb54cc3863fa519306dd4
Olorunbunmi Olajumoke <62801575+olajay-pro@users.noreply.github.com>
2021-08-11 10:33:08 +0100

    Update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdlib.h>
 
 char *create_buffer(char *file);
